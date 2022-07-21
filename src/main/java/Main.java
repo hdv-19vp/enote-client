fddd4323c6714a755d9d95c669cc3b9f784d19af
DiDiPhan <88415000+DiDiPhan@users.noreply.github.com>
2022-07-21 20:33:20 +0700

    init project enote client

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import ui.App;
+
+import java.io.File;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("enote client");
+    }
+}
