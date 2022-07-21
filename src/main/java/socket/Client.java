966cc4f88d63250211e973bb1ba5eed0e6b88ef5
DiDiPhan <88415000+DiDiPhan@users.noreply.github.com>
2022-07-21 21:19:36 +0700

    signIn signUp

--- src/main/java/socket/Client.java
+++ src/main/java/socket/Client.java
@@ -1,5 +1,55 @@
 package socket;
 
-public class Client {
+import java.io.*;
+import java.net.Socket;
 
+public class Client {
+    public final static String SERVER_IP = "127.0.0.1";
+    public final static int SERVER_PORT = 7;
+    public static Socket socket = null;
+    public static DataInputStream dis = null;
+    public static DataOutputStream dos = null;
+
+    public static boolean connect() {
+        try {
+            socket = new Socket(SERVER_IP, SERVER_PORT); // Connect to server
+            dis = new DataInputStream(socket.getInputStream());
+            dos = new DataOutputStream(socket.getOutputStream());
+            System.out.println("Connected: " + socket);
+
+            return true;
+        } catch (IOException e) {
+//            e.printStackTrace();
+
+            return false;
+        }
+    }
+
+    public static boolean signIn(String usr, String pwd){
+        try {
+            dos.writeUTF("signIn");
+            dos.writeUTF(usr);
+            dos.writeUTF(pwd);
+            String res = dis.readUTF();
+
+            return  res.equals("thanh cong");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean signUp(String usr, String pwd){
+        try {
+            dos.writeUTF("signUp");
+            dos.writeUTF(usr);
+            dos.writeUTF(pwd);
+            String res = dis.readUTF();
+
+            return  res.equals("thanh cong");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
--- src/main/java/socket/Main.java
+++ src/main/java/socket/Main.java
@@ -2,6 +2,8 @@ package socket;
 
 public class Main {
     public static void main(String[] args) {
-
+        Client.connect();
+        System.out.println(Client.signIn("phantuongvy", "123456"));
+        System.out.println(Client.signIn("phanvy", "123456"));
     }
 }
