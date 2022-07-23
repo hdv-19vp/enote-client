a05b61fcdb276a0971333f97f86460d60efb72b6
hoan3232 <76396996+hoan3232@users.noreply.github.com>
2022-07-24 00:35:16 +0700

    all client side functions complete and tested

--- src/main/java/group_02/client/socket/Main.java
+++ src/main/java/group_02/client/socket/Main.java
@@ -3,6 +3,6 @@ package group_02.client.socket;
 public class Main {
     public static void main(String[] args) {
         Client.connect();
-        System.out.println(Client.getEnoteList("phantuongvy"));
+        System.out.println(Client.getEnote("phantuongvy",5));
     }
 }
